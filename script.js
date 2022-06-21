3af64670a158ebb4c0382b23ff9e12265cc1aa2a
Muhthoriqas <muhthoriqas1719@gmail.com>
2022-06-21 18:44:54 +0700

    Update Logic Game

--- script.js
+++ script.js
@@ -16,9 +16,10 @@ let sumEl = document.getElementById("sum-el");
 let startAgain = document.getElementById("start");
 let player = {
   playerName: "Your",
-  Money: 100,
+  Money: 5000,
 };
 let playerMoney = document.getElementById("playerMoney");
+playerMoney.innerText = player.playerName + " Money: Rp." + player.Money;
 
 function nameP() {
   let name = document.getElementById("nama").value;
@@ -34,33 +35,55 @@ function nameP() {
 }
 
 function startGame() {
-  let firstCard = getRandomCard();
-  let secondCard = getRandomCard();
-  cards = [firstCard, secondCard];
-  sum = firstCard + secondCard;
-  isAlive = true;
-  notStart = false;
-  playerMoney.innerText = player.playerName + " Money: Rp." + player.Money;
+  let bet = document.getElementById("bet").value;
 
-  startAgain.textContent = "WANT TO PLAY AGAIN?";
-
-  gameBegin();
+  if (bet !== "" && player.Money > 0 && player.Money >= bet && bet !== 0) {
+    if (bet > 0) {
+      let firstCard = getRandomCard();
+      let secondCard = getRandomCard();
+      cards = [firstCard, secondCard];
+      sum = firstCard + secondCard;
+      isAlive = true;
+      notStart = false;
+      player.Money -= bet;
+      playerMoney.innerText = player.playerName + " Money: Rp." + player.Money;
+      startAgain.textContent = "WANT TO PLAY AGAIN?";
+      gameBegin();
+    } else {
+      alert("Your Bet is 0, Plese Set Your bet > 0!");
+    }
+  } else if (bet === "") {
+    alert("Set Your Bet First!");
+  } else if (player.Money === 0) {
+    alert("Your Money is = 0, Please Reset Money!");
+  } else {
+    alert("Your Money is < Bet, Please Reset Money or Change Bet!");
+  }
 }
+
 function gameBegin() {
+  hasBlackJack = false;
   cardEl.textContent = "YourCards: ";
   for (i = 0; i < cards.length; i++) {
     cardEl.textContent += cards[i] + " ";
   }
   sumEl.textContent = "Sum:" + sum;
-  if (sum <= 20) {
-    message = "Draw new Card?";
-  } else if (sum === 21) {
+  if (sum === 21) {
+    let bet = document.getElementById("bet").value;
     message = "Yo Got BlackJack!";
     hasBlackJack = true;
+    player.Money += bet * 6;
+    playerMoney.innerText = player.playerName + " Money: Rp." + player.Money;
+    document.getElementById("bet").value = "";
+  } else if (sum <= 20) {
+    message = "Draw new Card?";
   } else {
     message = "You Lose!";
+    document.getElementById("bet").value = "";
+
     isAlive = false;
   }
+
   messageEl.textContent = message;
   messageGameOver.textContent = "";
 }
@@ -69,10 +92,13 @@ function newCard() {
   if (notStart === true) {
     messageGameOver.textContent = "Please Start Game First!";
   } else if (isAlive === true) {
+    bet = document.getElementById("bet").value;
+
     if (hasBlackJack === false) {
       let card = getRandomCard();
       sum += card;
       cards.push(card);
+
       gameBegin();
     } else {
       startAgain.textContent = "WANT TO PLAY AGAIN ?";
@@ -92,5 +118,15 @@ function getRandomCard() {
     return 10;
   } else {
     return randomNumber;
+  }
+}
+function resetMoney() {
+  player.Money = 5000;
+  playerMoney.innerText = player.playerName + " Money: Rp." + player.Money;
+  if ((player.Money = 5000)) {
+    alert("Sucess Reset Money!!");
+    messageGameOver.textContent = "";
+  } else {
+    alert("Failed Reset Money!");
   }
 }
